b40da233e91fcefc61d3b57148815a2f6c4cf4da
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-12-09 11:43:51 -0800

    Add start of UI for bakery

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,7 +8,9 @@ namespace Bakery
     public static void Main()
     {
 
-      Console.WriteLine("\n" + "Welcome to my Bread and Butter" + "\n");
+      Console.WriteLine("\n" + "Who needs Yule Tidings when you have Carbs?");
+      Console.WriteLine("\n" + "Welcome to 'Bread and More(Bread)'" + "\n");
+      Console.WriteLine("Our Bakery is 'proof' that you 'knead' our deals!" + "\n");
 
     }
   }
