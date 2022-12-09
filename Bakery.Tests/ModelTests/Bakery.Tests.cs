9f268a9c2d51e04eee3ce532f21ba72b13ced4cf
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-12-09 09:38:25 -0800

    Add tests for bread and pastry

---
+++ Bakery.Tests/ModelTests/Bakery.Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+    [TestClass]
+    public class ItemTests
+    {
+        [TestMethod]
+        public void BreadConstructor_CreatesInstanceOfBread_Bread()
+        {
+            Bread newBread = new Bread (1);
+            Assert.AreEqual(typeof(Bread), newBread.GetType());
+        }
+        [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(1);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+    }
+}
