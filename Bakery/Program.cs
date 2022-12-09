37e91f72d0925622438b3472754b1f9c90d31016
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-12-09 12:02:38 -0800

    WIP Add more UI, lunch break

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -12,6 +12,14 @@ namespace Bakery
       Console.WriteLine("\n" + "Welcome to 'Bread and More(Bread)'" + "\n");
       Console.WriteLine("Our Bakery is 'proof' that you 'knead' our deals!" + "\n");
 
+      bool leave = false
+
+      while(!leave)
+
+      {
+        Console.WriteLine("Step on up! Prices are on the board, and you won't be bored of our prices!" + "\n" + "*-----------------------------*");
+      }
+
     }
   }
 }
