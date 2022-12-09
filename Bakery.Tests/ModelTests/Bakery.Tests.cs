4e6e202e2d0b8a6c73b052526785bb54ad8b1ebb
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-12-09 09:48:42 -0800

    Add test for adding pastry and bread

--- Bakery.Tests/ModelTests/Bakery.Tests.cs
+++ Bakery.Tests/ModelTests/Bakery.Tests.cs
@@ -8,17 +8,34 @@ namespace Bakery.Tests
     [TestClass]
     public class ItemTests
     {
-        [TestMethod]
-        public void BreadConstructor_CreatesInstanceOfBread_Bread()
-        {
-            Bread newBread = new Bread (1);
-            Assert.AreEqual(typeof(Bread), newBread.GetType());
-        }
-        [TestMethod]
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread (1);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+    [TestMethod]
+    
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
       Pastry newPastry = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
+    [TestMethod]
+    
+    public void GetPastryAndBreadAmount_ReturnsTotalAmount_Int()
+    {
+      
+      int pastryAmount = 4;
+      int breadAmount = 4;
+      Bread newBread = new Bread (breadAmount);
+      Pastry newPastry = new Pastry (breadAmount);
+     
+      int resultPastry = newPastry.PastryAmount;
+      int resultBread = newBread.BreadAmount;
+      int result = pastryAmount + breadAmount;
+     
+      Assert.AreEqual(pastryAmount + breadAmount, result);
     }
+  }
 }
