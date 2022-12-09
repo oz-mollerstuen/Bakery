071a3d5581b897736c29842f288fb4bc1364980a
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-12-09 14:13:44 -0800

    Add more UI for purchases

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,7 +8,7 @@ namespace Bakery
     public static void Main()
     {
 
-      Console.WriteLine("\n" + "Who needs Yule Tidings when you have Carbs?");
+      Console.WriteLine("\n" + "Who needs Yule Tidings when you can have Carbs?");
       Console.WriteLine("\n" + "Welcome to 'Bread and More(Bread)'" + "\n");
       Console.WriteLine("Our Bakery is 'proof' that you 'knead' our deals!" + "\n");
 
@@ -19,11 +19,11 @@ namespace Bakery
         Console.WriteLine("Step on up! Prices are on the board, and you won't be bored of our prices!" + "\n" + "*-----------------------------*");
         Console.WriteLine("\n" + "Bread: $3" + "\n" + "Pastries: $2" + "\n");
         Console.WriteLine("-------------------------------------------------" + "\n" + "Today's Specials: " + "\n");
-        Console.WriteLine("Bread ---> Buy Two, get one FREE" + "\n" "Pastries: ---> THREE for FIVE" + "\n");
+        Console.WriteLine("Bread ---> Buy Two, get one FREE" + "\n" + "Pastries: ---> THREE for FIVE" + "\n");
         Console.WriteLine("How many loaves of bread today?");
         string stringBreadAmount = Console.ReadLine();
         int breadAmount = int.Parse(stringBreadAmount);
-        Console.WriteLine("And how many pastries would you like?")
+        Console.WriteLine("And how many pastries would you like?");
         string stringPastryAmount = Console.ReadLine();
         int pastryAmount = int.Parse(stringPastryAmount);
         if (pastryAmount > 0 || breadAmount > 0) {
@@ -33,9 +33,23 @@ namespace Bakery
             int pastryPrice = newPastry.PastryTotal(pastryAmount);
 
             Console.WriteLine("Sounds like a deal! Lets take a look here..." + "\n");
-            Console.WriteLine(pastryAmount + " dollars for the pastries..." + "\n" + "and..." + "\n" + breadAmount + " dollars for bread!" + "\n")
+            Console.WriteLine(pastryPrice + " dollars for the pastries..." + "\n" + "and..." + "\n" + breadPrice + " dollars for bread!" + "\n");
             Console.WriteLine("Your total today will be: $" + newPastry.Total(breadPrice, pastryPrice));
-            
+        }
+        else {
+            Console.WriteLine("Either I'm losing it, or you forgot to get anything.");
+        }
+        Console.WriteLine("Is there anything else I can do for you today? [Y or N]" + "\n");
+        string userInp = Console.ReadLine().ToLower();
+        if (userInp == "y") {
+            leave = false;
+            Console.WriteLine("You got it!");
+        }
+        else {
+            Console.WriteLine("\n" + "Thank you for stopping by! We'll see you next time you 'roll' through!");
+            leave = true;
+        }
+
         }
 
         
@@ -43,4 +57,3 @@ namespace Bakery
 
     }
   }
-}
