ecd9b5410c700ec426d79da588c84161a4936053
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-12-09 16:10:18 -0800

    Add dictionary for bread choices

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -11,12 +11,16 @@ namespace Bakery.Models
     {
       BreadAmount = breadAmount;
     }
+  Dictionary<string, int> breadDict = new Dictionary<string, int>() {
+  {"1", 3},
+  {"2", 4},
+  {"3", 5}}; 
 
     public int BreadTotal(int breadAmount)
     {
       int breadCost = 0;
       if (BreadAmount <= 2) {
-        breadCost = BreadAmount * 3;
+        breadCost = BreadAmount * breadDict["bChoice"];
       } else {
         breadCost = (((BreadAmount - (BreadAmount % 3)) / 3) * 5) + ((BreadAmount % 3) * 2);
       }
